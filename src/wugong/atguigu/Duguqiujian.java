3ee4e02888d40d472edbfb227d19e8d2c853f38d
sunwenxue123 <2277600693@qq.com>
2018-11-28 20:46:26 +0800

    新武功

--- src/wugong/atguigu/Duguqiujian.java
+++ src/wugong/atguigu/Duguqiujian.java
@@ -3,8 +3,7 @@ package wugong.atguigu;
 public class Duguqiujian {
 
 	public static void main(String[] args) {
-		System.out.println("1111");
-		System.out.println("2222");
+		System.out.println("11111");
 	}
 
 }
